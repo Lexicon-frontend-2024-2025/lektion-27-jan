93b5fc19d7826bd30e888c2a578e60f4b9a869aa
Niklas Fähnrich <niklas.fahnrich@safeture.com>
2025-01-30 14:15:07 +0100

    fix bug in checkMovies where renderMoviesToUi is run before fetch is finish, and refactor renderMovieToUI function

--- start.js
+++ start.js
@@ -1,6 +1,10 @@
 console.log("koden är länkad");
 const BASE_URL = "https://ghibliapi.vercel.app/films/";
 
+checkMovies();
+
+// ########## functions below #########
+
 // få data från API:et
 async function fetchMovies() {
     try {
@@ -28,47 +32,52 @@ async function fetchMovies() {
 function checkMovies() {
     // kolla om det redan finns filmer i LS
     const all_movies = JSON.parse(localStorage.getItem("all_movies"));
+
     if (all_movies) {
         // i så fall: rendera från LS
         renderMoviesToUI(all_movies);
     } else {
         // annars hämta data från API
-        fetchMovies();
-        const updated_all_movies = JSON.parse(localStorage.getItem("all_movies"));
-        renderMoviesToUI(updated_all_movies);
+        fetchMovies().then(() => {
+            const updated_all_movies = JSON.parse(localStorage.getItem("all_movies"));
+            console.log(updated_all_movies)
+            renderMoviesToUI(updated_all_movies);
+
+            // Vi kopplar på en .then-metod för att "vänta" in fetchMovies innan vi anropar renderMoviesToUi som faktiskt förösker rendera ut saker i browsern.
+        });
     }
 };
-checkMovies();
 
 // rendera från LS till mitt UI
 function renderMoviesToUI(movies) {
-    console.log(movies);
     const moviesContainerEl = document.getElementById('movies-container');
-    movies.forEach((movie) => {
-        const movieContainerEl = document.createElement('article');
-        // på min artikel vill jag ha en eventlyssnare
-        movieContainerEl.addEventListener("click", () => {
-            console.log(movie);
-            // den skall slussa mig till en ny sida
-            window.location.href = `/movieView.html?id=${encodeURIComponent(movie.id)}`;
-        });
-        // skapa element för bilden
-        const movieImgEl = document.createElement('figure');
-        movieImgEl.style.backgroundImage = `url(${movie.image})`;
-        const rtScoreEl = document.createElement('p');
-        rtScoreEl.innerHTML = `<span class="rt-score">${movie.rt_score}%</span>`;
-        movieImgEl.appendChild(rtScoreEl);
-        // lägg till figure på movieContainerEl
-        movieContainerEl.appendChild(movieImgEl);
-        // lägg till titel och release date på filmkortet
-        const movieTitleEl = document.createElement('h3');
-        movieTitleEl.innerText = movie.title;
-        movieContainerEl.appendChild(movieTitleEl);
-        const releaseDateEl = document.createElement('p');
-        releaseDateEl.innerHTML = movie.release_date;
-        movieContainerEl.appendChild(releaseDateEl);
+    const movieCardsAsString = movies.map((m) => createMovieCard(m)).join("")
+    moviesContainerEl.innerHTML = movieCardsAsString;
 
-        // till sist lägger vi till nya elementet i vår HTML
-        moviesContainerEl.appendChild(movieContainerEl);
-    });
+    //     movieContainerEl.addEventListener("click", () => {
+    //         console.log(movie);
+    //         // den skall slussa mig till en ny sida
+    //         window.location.href = `/movieView.html?id=${encodeURIComponent(movie.id)}`;
+    //     });
+
+    
+
 };
+
+function createMovieCard (movie){
+    return `
+        <article>
+            <figure style="background-image: url(${movie.image})">
+                <p>
+                    <span class="rt-score">${movie.rt_score}%</span>
+                </p>
+            </figure>
+            <h3>
+                ${movie.title}
+            </h3>
+            <p>
+                ${movie.release_date}
+            </p>
+        </article>
+    `
+}
